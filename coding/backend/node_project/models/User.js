b03a6758224873a5854b5af0041ce0edb2e9f6b0
mac <muzi.ngobe@vacillate.co.za>
2020-09-05 19:33:02 +0200

    add user routes and a user Model

---
+++ coding/backend/node_project/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    minlength: 1,
+    validate: {
+      validator: validator.isEmail,
+      message: "{Value} is not a valid email",
+    },
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+
+  mobile: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  dob: {
+    type: Date,
+    required: true,
+  },
+
+  createdat: {
+    type: Date,
+  },
+  modifiedat: {
+    type: Date,
+    default: Date.now(),
+  },
+});
---
+++ coding/backend/node_project/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
+const { mongoUrl } = require("./keys");
+
+require("./models/User");
+const requireToken = require("./middleware/requireToken");
+const authRoutes = require("./routes/authRoutes");
+const profile = require("./routes/profile");
+
+app.use(bodyParser.json());
+app.use(authRoutes, profile);
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongodb");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("this is error", err);
+});
+
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  res.send("testing connection");
+});
+
+app.listen(PORT, () => {
+  console.log("server running: " + PORT);
+});
